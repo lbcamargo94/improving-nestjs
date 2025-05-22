d496d89c1105458f60a2731681ebb20d157ef7e9
Lucas Camargo <lucas.camargo@funcionalcorp.com.br>
2025-05-22 16:22:40 -0300

    feat(): created new users and auth modules

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './user.dto';
+import { v4 as uuid } from 'uuid';
+import { hashSync as bcryptHashSync } from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: UserDto[] = [
+    {
+      id: '10f88a09-05c3-46b6-8558-8e03147f5861',
+      username: 'user',
+      password: '123456',
+    },
+  ];
+
+  create(newUser: UserDto) {
+    newUser.id = uuid();
+    newUser.password = bcryptHashSync(newUser.password, 10);
+    this.users.push(newUser);
+
+    return this.users;
+  }
+}
